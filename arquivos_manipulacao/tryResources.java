f975ccfbba9215c482217b99180c4ed4203261c9
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2024-03-15 14:40:41 -0300

    usando bufferedreader e filereader de uma maneira bem melhor

--- arquivos_manipulacao/tryResources.java
+++ arquivos_manipulacao/tryResources.java
@@ -1,4 +1,23 @@
 package arquivos_manipulacao;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class tryResources {
+    public static void main(String[] args) {
+        String path = "c:\\temp\\in.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = br.readLine();
+
+            while (line != null){
+                System.out.println(line);
+                line = br.readLine();
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
