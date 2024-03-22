34393f18cad34f6eb392edd38622599930e4c3bc
Guilherme Alves Pinheiro <guiapinheiro29@gmail.com>
2024-03-21 23:12:10 -0300

    testando os tipos coringas

--- generics_set_map/generics/coringa/Program.java
+++ generics_set_map/generics/coringa/Program.java
@@ -1,20 +1,31 @@
 package generics_set_map.generics.coringa;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Program {
         public static void main(String[] args) {
-            List<Integer> myInts = Arrays.asList(5, 2, 10);
-            printList(myInts);
+            List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+            List<Double> myDoubles = Arrays.asList(3.14, 6.28);
+            List<Object> myObjs = new ArrayList<Object>();
 
-            List<?> myInterrogations = Arrays.asList("Maria", "Gui", 1, 2);
+            copy(myInts, myObjs);
+            printList(myObjs);
+            copy(myDoubles, myObjs);
+            printList(myObjs);
+        }
 
-            printList(myInterrogations);
-        }
-        public static void printList(List<?> list) {
-            for (Object obj : list) {
-                System.out.println(obj);
-        }
-    }
+            public static void copy(List<? extends Number> source, List<? super Number> destiny){
+                for (Number number : source){
+                    destiny.add(number);
+                }
+            }
+
+            public static void printList(List<?> list){
+                for (Object obj : list){
+                    System.out.print(obj + " ");
+                }
+                System.out.println();
+            }
 }
