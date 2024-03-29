030ce33412e34b913ce20012b69aa3c08f93e517
Guilherme Alves Pinheiro <guiapinheiro29@gmail.com>
2024-03-29 19:36:11 -0300

    update no banco

--- coursejdbc/src/application/Program.java
+++ coursejdbc/src/application/Program.java
@@ -1,48 +1,33 @@
 package src.application;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import db.DB;
-import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 public class Program {
     public static void main(String[] args) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
         Connection conn = null;
         PreparedStatement st = null;
-        try {
+
+        try{
             conn = DB.getConnection();
 
             st = conn.prepareStatement(
-                    "INSERT INTO seller "
-                    + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-                    + "VALUES(?, ?, ?, ?, ?)",
-                    Statement.RETURN_GENERATED_KEYS);
+                    "UPDATE seller "
+                    + "SET BaseSalary = BaseSalary + ? "
+                    + "WHERE DepartmentId = ? "
+            );
 
-            st.setString(1, "Carl Purple");
-            st.setString(2, "carl@gmail.com");
-            st.setDate(3, new java.sql.Date(sdf.parse("10/06/2003").getTime()));
-            st.setDouble(4, 3000.00);
-            st.setInt(5, 4);
+            st.setDouble(1, 200);
+            st.setInt(2, 2);
 
             int rowsAffected = st.executeUpdate();
 
-            if (rowsAffected > 0){
-                ResultSet rs = st.getGeneratedKeys();
-                while(rs.next()){
-                    int id = rs.getInt(1);
-                    System.out.println("Done! Id = " + id);
-                }
-            }
-            else {
-                System.out.println("No rows affected");
-            }
+            System.out.println("Done! Rows affected: " + rowsAffected);
         }
-        catch(SQLException e){
-            e.printStackTrace();
-        }
-        catch(ParseException e){
+        catch (SQLException e) {
             e.printStackTrace();
         }
         finally{
